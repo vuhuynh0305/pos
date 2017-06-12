d9e9b762d866ac00f9159afe69de188ea59ef007
vuhuynh0305 <mariohuynh0305@gmail.com>
2017-06-12 21:04:10 +0700

    update

--- js/main.js
+++ js/main.js
@@ -188,7 +188,7 @@ function slideService() {
                 items: 1
             },
             600: {
-                items: 3
+                items: 1
             },
             1000: {
                 items: 3
